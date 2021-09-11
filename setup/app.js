d89429c2f94eeb9b32daebdd04786ca4398ef5bc
Broc Hammett <bxrayhamm@gmail.com>
2021-09-10 22:58:39 -0500

    fixed the toggle button.

--- setup/app.js
+++ setup/app.js
@@ -1,2 +1,13 @@
-//using selectors inside the element
+// using selectors inside the element
 // traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn'); 
+    btn.addEventListener('click', function() {
+        question.classList.toggle('show-text');
+    });
+});
+
+
